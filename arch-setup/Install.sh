b27eab4b0b27139677af3ebf374871d080bef4de
RealStickman <mrcfrm01@protonmail.com>
2020-02-12 11:16:05 +0000

    Add username to add permission for logs

--- arch-setup/Install.sh
+++ arch-setup/Install.sh
@@ -1,1 +1,3 @@
 trizen -S libreoffice-fresh-de keepass steam steam-native-runtime lutris wireguard-arch wireguard-tools gnome-boxes ttf-ms-fonts ttf-tahoma ttf-vista-fonts
+
+usermod -aG log #username
