8032ed263e56796d1cf49a0f33c099be61fb299b
RealStickman <mrcfrm01@protonmail.com>
2020-02-13 19:40:35 +0000

    Now should launch i3 specific polybar config instead of standard config.

--- arch-config/.config/polybar/launch.sh
+++ arch-config/.config/polybar/launch.sh
@@ -21,10 +21,10 @@ case $desktop in
     i3)
     if type "xrandr" > /dev/null; then
       for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload mainbar-i3 -c ~/.config/polybar/config &
+        MONITOR=$m polybar --reload mainbar-i3 -c ~/.config/polybar/i3config &
       done
     else
-    polybar --reload mainbar-i3 -c ~/.config/polybar/config &
+    polybar --reload mainbar-i3 -c ~/.config/polybar/i3config &
     fi
     # second polybar at bottom
     # if type "xrandr" > /dev/null; then
