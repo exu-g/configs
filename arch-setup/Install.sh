8534206adfeaf100c43f07c3ed15da727ab7cb36
RealStickman <mrcfrm01@protonmail.com>
2020-02-19 20:54:05 +0000

    Added sudo

--- arch-setup/Install.sh
+++ arch-setup/Install.sh
@@ -3,4 +3,4 @@
 trizen -S libreoffice-fresh-de keepass steam steam-native-runtime lutris wireguard-arch wireguard-tools gnome-boxes ttf-ms-fonts ttf-tahoma ttf-vista-fonts discord bettergram cpu-x polybar youtube-dl debtap obs-studio audacity python-pip tk pyglet
 
 sudo pip3 install ffmpeg-normalize
-python3 -m pip install mutagen
+sudo python3 -m pip install mutagen
