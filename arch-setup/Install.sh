7264e4896d23a3c50d2e0bf882584b690a473124
RealStickman <mrcfrm01@protonmail.com>
2020-02-19 15:01:23 +0000

    Added tkinter install

--- arch-setup/Install.sh
+++ arch-setup/Install.sh
@@ -1,5 +1,5 @@
 !#/bin/bash
 
-trizen -S libreoffice-fresh-de keepass steam steam-native-runtime lutris wireguard-arch wireguard-tools gnome-boxes ttf-ms-fonts ttf-tahoma ttf-vista-fonts discord bettergram cpu-x polybar youtube-dl debtap obs-studio audacity python-pip
+trizen -S libreoffice-fresh-de keepass steam steam-native-runtime lutris wireguard-arch wireguard-tools gnome-boxes ttf-ms-fonts ttf-tahoma ttf-vista-fonts discord bettergram cpu-x polybar youtube-dl debtap obs-studio audacity python-pip tk
 
 sudo pip3 install ffmpeg-normalize
