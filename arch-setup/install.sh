787eec0f4efbd5e4032b78bfca291f07673068d1
RealStickman <mrcfrm01@protonmail.com>
2020-02-24 14:58:59 +0000

    Added fish so my config works on a fresh system.

--- arch-setup/install.sh
+++ arch-setup/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-trizen -S libreoffice-fresh-de keepass steam steam-native-runtime lutris wireguard-arch wireguard-tools gnome-boxes ttf-ms-fonts ttf-tahoma ttf-vista-fonts discord bettergram cpu-x polybar youtube-dl debtap obs-studio audacity python-pip tk pyglet multibootusb picard hunspell hunspell-de hyphen hyphen-de
+trizen -S libreoffice-fresh-de keepass steam steam-native-runtime lutris wireguard-arch wireguard-tools gnome-boxes ttf-ms-fonts ttf-tahoma ttf-vista-fonts discord bettergram cpu-x polybar youtube-dl debtap obs-studio audacity python-pip tk pyglet multibootusb picard hunspell hunspell-de hyphen hyphen-de fish
 
 sudo pip3 install ffmpeg-normalize
 sudo python3 -m pip install mutagen
