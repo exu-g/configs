33574b9748c916d4e2c5fee8c6d78a6ae653f81b
RealStickman <mrcfrm01@protonmail.com>
2020-02-27 18:05:53 +0000

    Update install.sh

--- arch-setup/install.sh
+++ arch-setup/install.sh
@@ -2,6 +2,9 @@
 
 trizen -S libreoffice-fresh-de keepass steam steam-native-runtime lutris wireguard-arch wireguard-tools gnome-boxes ttf-ms-fonts ttf-tahoma ttf-vista-fonts discord bettergram cpu-x polybar youtube-dl debtap obs-studio audacity python-pip tk pyglet picard hunspell hunspell-de hyphen hyphen-de fish
 
+#install if nvidia drivers are used
+#sudo pacman -S nvidia nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader
+
 sudo pip3 install ffmpeg-normalize
 sudo python3 -m pip install mutagen
 
