341ce4514830a35392654699852e1c45120d3189
RealStickman <mrcfrm01@protonmail.com>
2020-02-28 21:42:11 +0000

    Comments

--- arch-config/.config/scripts/download-arcolinux-config.sh
+++ arch-config/.config/scripts/download-arcolinux-config.sh
@@ -3,24 +3,32 @@
 #change to home (does not show in terminal)
 cd $HOME
 
+#remove old installs
 rm -rf ~/arcolinux-config
 
+#clone this repo
 git clone https://gitlab.com/RealStickman/arcolinux-config.git &&
 
+#copy stuff
 cp -r ~/arcolinux-config/.config/ ~/ &&
 cp ~/arcolinux-config/.bashrc ~/ &&
+#copy commands
+cp ~/arcolinux-config/Commands ~/Dokumente
 
+#make bash scripts executable
 chmod +x ~/.config/polybar/launch.sh
 chmod +x ~/.config/scripts/startup.sh
 chmod +x ~/.config/scripts/download-arcolinux-config.sh
 
-cp ~/arcolinux-config/Commands ~/Dokumente
-
+#remove downloaded folder
 rm -rf ~/arcolinux-config
 
+#restart i3 in place
 i3 restart
 
+#output
 echo Finished updating everything!
 echo Launching new shell!
 
+#new shell to reload .bashrc
 exec bash
