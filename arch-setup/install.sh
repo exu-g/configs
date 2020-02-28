aed858e71c54c9110928dfbb8c5413be6d7b060f
RealStickman <mrcfrm01@protonmail.com>
2020-02-28 21:28:53 +0000

    Changes to home directory of active user when launching the script.

--- arch-setup/install.sh
+++ arch-setup/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd $HOME
 trizen -S libreoffice-fresh-de keepass steam steam-native-runtime lutris wireguard-arch wireguard-tools gnome-boxes ttf-ms-fonts ttf-tahoma ttf-vista-fonts discord bettergram cpu-x polybar youtube-dl debtap obs-studio audacity python-pip tk pyglet picard hunspell hunspell-de hyphen hyphen-de fish psensor stress transmission-gtk
 
 #install if nvidia drivers are used
@@ -12,6 +12,12 @@ sudo python3 -m pip install mutagen
 #  sudo ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
 #  sudo ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
 #  sudo ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
+
+#pcloud
+https://www.pcloud.com/how-to-install-pcloud-drive-linux.html?download=electron-64
+cp ~/Downloads/pcloud ~/
+chmod +x ~/pcloud
+~/pcloud
 
 #execute this in the home folder!!
 git clone https://gitlab.com/RealStickman/arcolinux-config
