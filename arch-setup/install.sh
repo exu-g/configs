d55bb4828bc7131e6e1502cb4cf5acab2513e918
RealStickman <mrcfrm01@protonmail.com>
2020-02-29 14:16:05 +0000

    #!/bin/bash echo Added echos

--- arch-setup/install.sh
+++ arch-setup/install.sh
@@ -3,16 +3,20 @@
 #change to home (does not show in terminal)
 cd $HOME
 
-trizen -S libreoffice-fresh-de keepass steam steam-native-runtime lutris wireguard-arch wireguard-tools gnome-boxes ttf-ms-fonts ttf-tahoma ttf-vista-fonts discord bettergram cpu-x polybar youtube-dl debtap obs-studio audacity python-pip tk pyglet picard hunspell hunspell-de hyphen hyphen-de fish psensor stress transmission-gtk gimp krita blender
+trizen -S libreoffice-fresh-de keepass steam steam-native-runtime lutris wireguard-arch wireguard-tools gnome-boxes ttf-ms-fonts ttf-tahoma ttf-vista-fonts discord bettergram cpu-x polybar youtube-dl debtap obs-studio audacity python-pip tk picard hunspell hunspell-de hyphen hyphen-de fish psensor stress transmission-gtk gimp krita blender
+echo Installed packages
 
 #update stuff
 trizen -Syyu
+echo Updated packages
 
 #install if nvidia drivers are used
 #sudo pacman -S nvidia nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader
 
 sudo pip3 install ffmpeg-normalize
 sudo python3 -m pip install mutagen
+sudo pip install pyglet
+echo Installed python modules
 
 #trizen -S ttf-dejavu ttf-liberation noto-fonts
 #  sudo ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
@@ -20,18 +24,21 @@ sudo python3 -m pip install mutagen
 #  sudo ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
 
 #pcloud
-https://www.pcloud.com/how-to-install-pcloud-drive-linux.html?download=electron-64
-cp ~/Downloads/pcloud ~/
+wget https://p-ams1.pcloud.com/cBZetST9hZtTO10SZZZm5zha7Z2ZZaERZkZVcA5VZAkZJFZL5ZqZlJZ5XZX0Z2JZpFZ6JZ9XZG5Z5kZg5Z1EnhkZC0y6XrVckg0fVXUvkxtywbSPBEH7/pcloud
 chmod +x ~/pcloud
 ~/pcloud
+echo Finished downloading pcloud
 
 #Changes to home folder automatically now, no need to be extra careful anymore.
 git clone https://gitlab.com/RealStickman/arcolinux-config
 cp ~/arcolinux-config/.bashrc ~/
 chmod +x ~/arcolinux-config/.config/scripts/download-arcolinux-config.sh
+echo Finished downloading config
 
 #cleanup
 rm -rf ~/arcolinux-setup
+echo Removed old setup files
 
 #downloading config
+echo Setting up config
 ~/arcolinux-config/.config/scripts/download-arcolinux-config.sh
