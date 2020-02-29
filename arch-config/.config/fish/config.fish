f5c98f7ab2655ade6c6572b424b41469bbb9561d
RealStickman <mrcfrm01@protonmail.com>
2020-02-29 14:26:24 +0000

    Update config.fish

--- arch-config/.config/fish/config.fish
+++ arch-config/.config/fish/config.fish
@@ -53,7 +53,7 @@ alias update='trizen -Syyu --noconfirm'
 #alias upall="yay -Syu --noconfirm"
 
 #trizen updates everything
-alias upall="trizen -Syyu --noconfirm" && upconf
+alias upall="trizen -Syyu --noconfirm" && "/home/marc/.config/scripts/download-arcolinux-config.sh"
 
 #ps
 alias ps="ps auxf"
