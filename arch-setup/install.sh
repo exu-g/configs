bd28ccd68750c9f5e70aa3ea9a5b798afc57cca5
RealStickman <mrcfrm01@protonmail.com>
2020-03-01 09:48:06 +0000

    Pulls mangohud from git instead of using AUR.

--- arch-setup/install.sh
+++ arch-setup/install.sh
@@ -3,7 +3,7 @@
 #change to home (does not show in terminal)
 cd $HOME
 
-trizen -S libreoffice-fresh-de keepass steam steam-native-runtime lutris wireguard-arch wireguard-tools gnome-boxes ttf-ms-fonts ttf-tahoma ttf-vista-fonts discord bettergram cpu-x polybar youtube-dl debtap obs-studio audacity python-pip tk picard hunspell hunspell-de hyphen hyphen-de fish psensor stress transmission-gtk gimp krita blender mangohud-git
+trizen -S libreoffice-fresh-de keepass steam steam-native-runtime lutris wireguard-arch wireguard-tools gnome-boxes ttf-ms-fonts ttf-tahoma ttf-vista-fonts discord bettergram cpu-x polybar youtube-dl debtap obs-studio audacity python-pip tk picard hunspell hunspell-de hyphen hyphen-de fish psensor stress transmission-gtk gimp krita blender
 
 #install wine
 trizen -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
@@ -13,6 +13,9 @@ echo Installed packages
 trizen -Syyu
 echo Updated packages
 
+#pull mangohud
+git clone --recurse-submodules https://github.com/flightlessmango/MangoHud.git
+./MangoHud/build.sh install
 #install if nvidia drivers are used
 #sudo pacman -S nvidia nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader
 
