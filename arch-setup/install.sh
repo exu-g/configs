216902c68b2a78b98a64efaae64cc3465def169c
RealStickman <mrcfrm01@protonmail.com>
2020-03-04 12:30:42 +0000

    Update install.sh

--- arch-setup/install.sh
+++ arch-setup/install.sh
@@ -32,10 +32,10 @@ echo Installed python modules
 #  sudo ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
 
 #pcloud
-wget https://p-ams1.pcloud.com/cBZetST9hZtTO10SZZZm5zha7Z2ZZaERZkZVcA5VZAkZJFZL5ZqZlJZ5XZX0Z2JZpFZ6JZ9XZG5Z5kZg5Z1EnhkZC0y6XrVckg0fVXUvkxtywbSPBEH7/pcloud
-chmod +x ~/pcloud
-~/pcloud
-echo Finished downloading pcloud
+#wget https://p-ams1.pcloud.com/cBZetST9hZtTO10SZZZm5zha7Z2ZZaERZkZVcA5VZAkZJFZL5ZqZlJZ5XZX0Z2JZpFZ6JZ9XZG5Z5kZg5Z1EnhkZC0y6XrVckg0fVXUvkxtywbSPBEH7/pcloud
+#chmod +x ~/pcloud
+#~/pcloud
+#echo Finished downloading pcloud
 
 #Changes to home folder automatically now, no need to be extra careful anymore.
 git clone https://gitlab.com/RealStickman/arcolinux-config
