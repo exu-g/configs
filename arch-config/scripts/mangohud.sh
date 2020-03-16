b3efe9497baa78b7b2acf9c025bd7d51ee353643
RealStickman <mrcfrm01@protonmail.com>
2020-03-16 14:25:45 +0000

    Update mangohud.sh

--- arch-config/scripts/mangohud.sh
+++ arch-config/scripts/mangohud.sh
@@ -2,4 +2,4 @@
 
 git clone --recurse-submodules https://github.com/flightlessmango/MangoHud.git
 cd ~/MangoHud
-bash build.sh package & bash build.sh install
+bash build.sh package #bash build.sh install
