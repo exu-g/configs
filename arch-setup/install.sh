b4a45f41245b72df074f883c84f8ad974fe6b957
RealStickman <mrcfrm01@protonmail.com>
2020-04-16 06:51:26 +0000

    Update install.sh

--- arch-setup/install.sh
+++ arch-setup/install.sh
@@ -35,22 +35,14 @@ echo Installed python modules
 #  sudo ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
 #  sudo ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
 
-#pcloud
-#wget https://p-ams1.pcloud.com/cBZetST9hZtTO10SZZZm5zha7Z2ZZaERZkZVcA5VZAkZJFZL5ZqZlJZ5XZX0Z2JZpFZ6JZ9XZG5Z5kZg5Z1EnhkZC0y6XrVckg0fVXUvkxtywbSPBEH7/pcloud
-#chmod +x ~/pcloud
-#~/pcloud
-#echo Finished downloading pcloud
-
 #Changes to home folder automatically now, no need to be extra careful anymore.
-#git clone https://gitlab.com/RealStickman/arcolinux-config
-#cp ~/arcolinux-config/.bashrc ~/
-#chmod +x ~/arcolinux-config/.config/scripts/download-arcolinux-config.sh
-#echo Finished downloading config
+git clone https://gitlab.com/RealStickman-arcolinux/arcolinux-config
+echo Finished downloading config
 
 #cleanup
 rm -rf ~/arcolinux-setup
 echo Removed old setup files
 
 #downloading config
-#echo Setting up config
-#~/arcolinux-config/.config/scripts/download-arcolinux-config.sh
+echo Setting config
+bash ~/arcolinux-config/install.sh
