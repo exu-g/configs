0802943e8f61ce10c17cd98f1a65b05d9d5a85ec
RealStickman <mrcfrm01@protonmail.com>
2020-04-21 08:16:27 +0000

    Update install.sh

--- arch-config/install.sh
+++ arch-config/install.sh
@@ -5,41 +5,6 @@
 #change to home (does not show in terminal)
 cd $HOME
 
-#remove old installs
-#rm -rf ~/arcolinux-config
-
-#clone this repo
-#git clone https://gitlab.com/RealStickman-arcolinux/arcolinux-config.git &&
-
-#copy stuff
-#cp -r ~/arcolinux-config/.config/ ~/ &&
-#cp ~/arcolinux-config/.bashrc ~/ &&
-#copy commands
-#cp ~/arcolinux-config/Commands ~/Dokumente
-#copy scripts
-#cp -r ~/arcolinux-config/scripts/ ~/
-#copy stuff to /etc
-#sudo cp -r ~/arcolinux-config/etc /
-
-#make bash scripts executable
-#chmod +x ~/.config/polybar/launch.sh
-#chmod +x ~/.config/scripts/startup.sh
-#chmod +x ~/.config/i3/scripts/i3exit.sh
-#chmod +x ~/scripts/arcolinux-config.sh
-
-#remove downloaded folder
-#rm -rf ~/arcolinux-config
-
-#restart i3 in place
-#i3 restart
-
-#output
-#echo Finished updating everything!
-#echo Launching new shell!
-
-#new shell to reload .bashrc and fish config
-#exec bash
-
 echo Launching arcolinux-config.sh
 
 #make executable & launch arcolinux-config.sh
