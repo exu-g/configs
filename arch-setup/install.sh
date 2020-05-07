a8670c88b09537004bc2b270c04c460ed927deb4
RealStickman <mrcfrm01@protonmail.com>
2020-05-07 06:43:48 +0000

    Changed Trizen for pacman & yay

--- arch-setup/install.sh
+++ arch-setup/install.sh
@@ -10,10 +10,16 @@ sudo pacman -Rns evolution catfish geany vim keepass gnome-boxes sublime-text-de
 trizen -Syyu
 echo Updated packages
 
-echo Installing missing programs
-trizen -S --needed libreoffice-fresh-de steam steam-native-runtime i3-gaps lutris virt-manager ttf-ms-fonts ttf-tahoma ttf-vista-fonts discord bettergram cpu-x polybar youtube-dl blender krita
-trizen -S --needed termite neofetch arandr firefox variety wget sublime-text-dev picom handbrake nutty bitwarden woeusb nohang-git pcloud-drive lightdm-webkit-theme-aether gimp stress
-trizen -S --needed oneko debtap obs-studio audacity python-pip tk picard hunspell hunspell-de hyphen hyphen-de fish psensor transmission-gtk rig smartmontools thunderbird code gimp-plugin-registry
+#echo Installing missing programs
+#trizen -S --needed libreoffice-fresh-de steam steam-native-runtime i3-gaps lutris virt-manager ttf-ms-fonts ttf-tahoma ttf-vista-fonts discord bettergram cpu-x polybar youtube-dl blender krita termite neofetch arandr firefox variety wget sublime-text-dev picom handbrake nutty bitwarden woeusb nohang-git pcloud-drive lightdm-webkit-theme-aether gimp stress debtap obs-studio audacity python-pip tk picard hunspell hunspell-de hyphen hyphen-de fish psensor transmission-gtk rig smartmontools thunderbird code gimp-plugin-registry
+
+echo Installing official programs
+sudo pacman -S --needed xfce4 arandr libreoffice-fresh-de steam steam-native-runtime i3-gaps lutris virt-manager discord youtube-dl blender krita termite neofetch firefox wget picom handbrake gimp stress obs-studio audacity python-pip hunspell hunspell-de hyphen hyphen-de fish psensor transmission-qt smartmontools thunderbird ffmpeg jre-openjdk thunar code gtk-engine-murrine
+
+#AUR
+echo Installing AUR programs
+yay -S --needed ttf-ms-fonts ttf-tahoma ttf-vista-fonts bettergram cpu-x polybar nutty bitwarden woeusb nohang-git lightdm-webkit-theme-aether debtap rig gimp-plugin-registry
+yay -S --needed pcloud-drive
 
 #install wine
 pacman -S --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
