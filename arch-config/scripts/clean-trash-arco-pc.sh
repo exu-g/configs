0559243d7842fc86c90c9d4b54001fd040625bf8
RealStickman <mrcfrm01@protonmail.com>
2020-05-16 10:12:44 +0000

    Verbose

--- arch-config/scripts/clean-trash-arco-pc.sh
+++ arch-config/scripts/clean-trash-arco-pc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rm -rf $HOME/.local/share/Trash
-rm -rf /mnt/1d90c4d5-21d2-4455-bb4a-814de8496744/.Trash-1000/
+rm -rvf $HOME/.local/share/Trash
+rm -rvf /mnt/1d90c4d5-21d2-4455-bb4a-814de8496744/.Trash-1000/
