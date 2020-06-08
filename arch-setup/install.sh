e482638619682d4043e3b9a1b7f1c9d37fcb8ee5
RealStickman <mrcfrm01@protonmail.com>
2020-06-08 15:45:25 +0000

    Update install.sh

--- arch-setup/install.sh
+++ arch-setup/install.sh
@@ -4,7 +4,7 @@
 cd $HOME
 
 #uninstall
-sudo pacman -Rns evolution catfish geany vim keepass gnome-boxes sublime-text-dev atom adwaita-icon-theme arcolinux-i3wm-git arcolinux-tweak-tool-git arcolinux-welcome-app-git clonezilla evolution-data-server numix-circle-arc-icons-git numix-circle-icon-theme-git numix-gtk-theme-git numix-icon-theme-git oh-my-zsh-git pamac-aur qbittorrent vivaldi vlc code
+sudo pacman -Rns evolution catfish geany vim keepass gnome-boxes sublime-text-dev atom adwaita-icon-theme arcolinux-i3wm-git arcolinux-tweak-tool-git arcolinux-welcome-app-git clonezilla evolution-data-server numix-circle-arc-icons-git numix-circle-icon-theme-git numix-gtk-theme-git numix-icon-theme-git oh-my-zsh-git pamac-aur qbittorrent vivaldi vlc code baka-mplayer
 
 #update stuff
 yay -Syyu
@@ -14,7 +14,7 @@ echo Updated packages
 #trizen -S --needed libreoffice-fresh-de steam steam-native-runtime i3-gaps lutris virt-manager ttf-ms-fonts ttf-tahoma ttf-vista-fonts discord bettergram cpu-x polybar youtube-dl blender krita termite neofetch arandr firefox variety wget sublime-text-dev picom handbrake nutty bitwarden woeusb nohang-git pcloud-drive lightdm-webkit-theme-aether gimp stress debtap obs-studio audacity python-pip tk picard hunspell hunspell-de hyphen hyphen-de fish psensor transmission-gtk rig smartmontools thunderbird code gimp-plugin-registry
 
 echo Installing official programs
-sudo pacman -S --needed xfce4 arandr qemu libreoffice-fresh-de steam steam-native-runtime i3-gaps lutris virt-manager discord youtube-dl blender krita termite neofetch firefox wget picom handbrake gimp stress obs-studio audacity python-pip hunspell hunspell-de hyphen hyphen-de fish psensor transmission-qt smartmontools thunderbird ffmpeg jre-openjdk thunar gtk-engine-murrine iperf3 baka-mplayer nload languagetool
+sudo pacman -S --needed xfce4 arandr qemu libreoffice-fresh-de steam steam-native-runtime i3-gaps lutris virt-manager discord youtube-dl blender krita termite neofetch firefox wget picom handbrake gimp stress obs-studio audacity python-pip hunspell hunspell-de hyphen hyphen-de fish psensor transmission-qt smartmontools thunderbird ffmpeg jre-openjdk thunar gtk-engine-murrine iperf3 celluloid nload languagetool
 
 #AUR
 echo Installing AUR programs
