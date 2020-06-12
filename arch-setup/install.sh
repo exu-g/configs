3569ef9f69b4e92b292d79fd622c465a73b6205b
RealStickman <mrcfrm01@protonmail.com>
2020-06-12 14:36:17 +0000

    Removed some unneeded lines

--- arch-setup/install.sh
+++ arch-setup/install.sh
@@ -115,16 +115,8 @@ echo Installing wine
 pacman -S --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
 echo Installed wine
 
-#pull mangohud
-#echo Installing MangoHud
-#git clone --recurse-submodules https://github.com/flightlessmango/MangoHud.git
-#./MangoHud/build.sh install
-#echo Installed MangoHud
-
 echo Installing python modules
 sudo pip3 install ffmpeg-normalize
-#sudo python3 -m pip install mutagen
-#sudo pip install pyglet
 echo Installed python modules
 
 #Changes to home folder automatically now, no need to be extra careful anymore.
