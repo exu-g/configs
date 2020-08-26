782c2ffd2e741429adcc8a83aa85d8db2452a6ee
RealStickman <mrcfrm01@protonmail.com>
2020-08-26 19:40:24 +0000

    Cleaned up a lot

--- arch-config/.config/fish/config.fish
+++ arch-config/.config/fish/config.fish
@@ -1,9 +1,6 @@
 #update config
 alias upconf='~/scripts/arcolinux-config.sh'
 
-#mount nfs drives
-alias nfspi='sudo mount 192.168.1.113:/mnt/ssd /mnt/nfs/ssd/ && sudo mount 192.168.1.113:/mnt/hdd /mnt/nfs/hdd/ && echo Success!'
-
 #list
 alias ls='ls --color=auto'
 alias la='ls -a'
@@ -13,83 +10,24 @@ alias l.="ls -A | egrep '^\.'"
 
 # fix obvious typo's
 alias cd..='cd ..'
-alias pdw="pwd"
-#alias udpate='sudo pacman -Syyu'
-alias udpate='yay -Syu'
-#alias upate='sudo pacman -Syyu'
-alias upate='yay -Syu'
 
 ## Colorize the grep command output for ease of use (good for log files)##
 alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
 
-# readable output
-alias df='df -h'
-
-# pacman unlock
-alias unlock="sudo rm /var/lib/pacman/db.lck"
-
-# free
-alias free="free -mt"
-
-# use all cores
-alias uac="sh ~/.bin/main/000*"
-
 # continue download
 alias wget="wget -c"
 
-#userlist
-alias userlist="cut -d: -f1 /etc/passwd"
-
-#merge new settings
-alias merge="xrdb -merge ~/.Xresources"
-
 # Aliases for software managment
-# pacman or pm
 alias pacman='sudo pacman --color auto'
-#alias update='sudo pacman -Syyu'
-#alias update='trizen -Syyu --noconfirm'
 alias update='yay -Syu'
 
-# yay as aur helper - updates everything
-#alias pksyua="yay -Syu --noconfirm"
-#alias upall="yay -Syu --noconfirm"
-
 #trizen updates everything
-#alias upall="trizen -Syyu --noconfirm && ~/.config/scripts/download-arcolinux-config.sh"
-alias upall="trizen -Syyu --noconfirm && bash ~/scripts/arcolinux-config.sh"
-
-#ps
-alias ps="ps auxf"
-alias psgrep="ps aux | grep -v grep | grep -i -e VSZ -e"
+alias upall="trizen -Syyu --noconfirm && bash $HOME/scripts/arcolinux-config.sh"
 
 #grub update
 alias update-grub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
-
-#improve png
-#alias fixpng="find . -type f -name "*.png" -exec convert {} -strip {} \;"
-
-#add new fonts
-alias update-fc='sudo fc-cache -fv'
-
-#copy/paste all content of /etc/skel over to home folder - backup of config created - beware
-#alias skel='cp -Rf ~/.config ~/.config-backup-$(date +%Y.%m.%d-%H.%M.%S) && cp -rf /etc/skel/* ~'
-#backup contents of /etc/skel to hidden backup folder in home/user
-#alias bupskel='cp -Rf /etc/skel ~/.skel-backup-$(date +%Y.%m.%d-%H.%M.%S)'
-
-#copy bashrc-latest over on bashrc - cb= copy bashrc
-alias cb="cp ~/.bashrc-latest ~/.bashrc && source ~/.bashrc && sudo cp /etc/skel/.bashrc-latest /etc/skel/.bashrc"
-
-#quickly kill conkies
-alias kc='killall conky'
-
-#hardware info --short
-alias hw="hwinfo --short"
-
-#skip integrity check
-alias yayskip='yay -S --mflags --skipinteg'
-alias trizenskip='trizen -S --skipinteg'
 
 #check vulnerabilities microcode
 alias microcode='grep . /sys/devices/system/cpu/vulnerabilities/*'
@@ -103,14 +41,6 @@ alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pac
 #mounting the folder Public for exchange between host and guest on virtualbox
 alias vbm="sudo mount -t vboxsf -o rw,uid=1000,gid=1000 Public /home/$USER/Public"
 
-#shopt
-#shopt -s autocd # change to named directory
-#shopt -s cdspell # autocorrects cd misspellings
-#shopt -s cmdhist # save multi-line commands in history as single line
-#shopt -s dotglob
-#shopt -s histappend # do not overwrite history
-#shopt -s expand_aliases # expand aliases
-
 #youtube-dl
 alias yta-aac="youtube-dl --extract-audio --audio-format aac "
 alias yta-best="youtube-dl --extract-audio --audio-format best "
@@ -123,27 +53,11 @@ alias yta-wav="youtube-dl --extract-audio --audio-format wav "
 
 alias ytv-best="youtube-dl -f bestvideo+bestaudio "
 
-#Recent Installed Packages
-alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -100"
-
 #Cleanup orphaned packages
 alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
 
 #get the error messages from journalctl
 alias jctl="journalctl -p 3 -xb"
-
-#nano
-alias nlightdm="sudo nano /etc/lightdm/lightdm.conf"
-alias npacman="sudo nano /etc/pacman.conf"
-alias ngrub="sudo nano /etc/default/grub"
-alias nmkinitcpio="sudo nano /etc/mkinitcpio.conf"
-alias nslim="sudo nano /etc/slim.conf"
-alias noblogout="sudo nano /etc/oblogout.conf"
-alias nmirrorlist="sudo nano /etc/pacman.d/mirrorlist"
-
-#shutdown or reboot
-alias ssn="sudo shutdown now"
-alias sr="sudo reboot"
 
 #cpu-x as sudo
 alias cpu-x="sudo cpu-x"
