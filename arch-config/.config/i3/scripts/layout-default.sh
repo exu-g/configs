0046bd0e37fa9d02b1815c0c7ca9ae1f2d8bd195
RealStickman <mrcfrm01@protonmail.com>
2020-08-29 12:27:08 +0000

    Changes mode to default again

--- arch-config/.config/i3/scripts/layout-default.sh
+++ arch-config/.config/i3/scripts/layout-default.sh
@@ -13,3 +13,5 @@ i3-msg 'workspace 2; layout tabbed; exec discord'
 i3-msg 'workspace 2; layout tabbed; exec mirage'
 
 i3-msg 'workspace 2; layout tabbed; exec bettergram'
+
+i3-msg 'mode "default"'
