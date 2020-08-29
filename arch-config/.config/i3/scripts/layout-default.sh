fa06ecd63b316fa8a619fc01c54d21746fe43e94
RealStickman <mrcfrm01@protonmail.com>
2020-08-29 12:11:31 +0000

    Upload New File

---
+++ arch-config/.config/i3/scripts/layout-default.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+i3-msg 'workspace 1; exec termite'
+
+i3-msg 'workspace 3; exec liferea'
+
+i3-msg 'workspace 2; layout tabbed; exec firefox'
+
+i3-msg 'workspace 2; layout tabbed; exec thunderbird'
+
+i3-msg 'workspace 2; layout tabbed; exec discord'
+
+i3-msg 'workspace 2; layout tabbed; exec mirage'
+
+i3-msg 'workspace 2; layout tabbed; exec bettergram'
