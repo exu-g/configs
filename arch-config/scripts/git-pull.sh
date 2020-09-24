89a6473c57145486887d46596d974a476d58e893
RealStickman <mrcfrm01@protonmail.com>
2020-09-24 08:52:29 +0200

    Pull changes from my public repositories.

---
+++ arch-config/scripts/git-pull.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd "$HOME/GitProjects/config" || exit
+git pull
+
+cd "$HOME/GitProjects/grab" || exit
+git pull
+
+cd "$HOME/GitProjects/setup" || exit
+git pull
+
+cd "$HOME/GitProjects/setup-pinephone" || exit
+git pull
