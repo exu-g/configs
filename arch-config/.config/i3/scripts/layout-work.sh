7e2e34a261b07a65c974bac8c116a946a9e1cfdc
RealStickman <mrcfrm01@protonmail.com>
2020-09-26 17:13:26 +0200

    Added work layout

---
+++ arch-config/.config/i3/scripts/layout-work.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+i3-msg 'workspace 1; exec termite'
+
+i3-msg 'workspace 2; layout tabbed; exec firefox'
+
+i3-msg 'workspace 2; layout tabbed; exec thunderbird'
+
+i3-msg 'workspace 2; layout tabbed; exec slack'
+
+i3-msg 'workspace 2; layout tabbed; exec teams'
+
+i3-msg 'mode "default"'
