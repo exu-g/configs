d73197879c3c6f6cea5e08346a2d129708ba669f
RealStickman <mrcfrm01@protonmail.com>
2020-09-28 18:37:18 +0200

    Enabling minimap on startup. Use SPC-t-m to toggle.

--- arch-config/.doom.d/config.el
+++ arch-config/.doom.d/config.el
@@ -52,3 +52,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; Enables the minimap by default. Use SPC-t-m to toggle
+(minimap-mode 1)
