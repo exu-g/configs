b9b0d230e474b875cf896ddc105a1dc6fa3f1640
RealStickman <mrcfrm01@protonmail.com>
2020-10-11 12:48:21 +0200

    Added pychecker

--- arch-setup/install.sh
+++ arch-setup/install.sh
@@ -460,7 +460,7 @@ fi
 #doom-emacs
 if [ $in_doomemacs -eq 1 ]; then
     echo "Installing doom-emacs"
-    yay -S --needed --noconfirm git emacs ripgrep fd pandoc shellcheck python-pipenv python-isort python-pytest python-rednose
+    yay -S --needed --noconfirm git emacs ripgrep fd pandoc shellcheck python-pipenv python-isort python-pytest python-rednose pychecker
     git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
     ~/.emacs.d/bin/doom install
     export PATH="$PATH":$HOME/.emacs.d/bin
