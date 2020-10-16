3cd1c84826d335d8556f3c627633078425d6c817
RealStickman <mrcfrm01@protonmail.com>
2020-10-16 17:36:59 +0200

    Added texlive for pdflatex engine

--- arch-setup/install.sh
+++ arch-setup/install.sh
@@ -462,7 +462,7 @@ fi
 #doom-emacs
 if [ $in_doomemacs -eq 1 ]; then
     echo "Installing doom-emacs"
-    yay -S --needed --noconfirm git emacs ripgrep fd pandoc shellcheck python-pipenv python-isort python-pytest python-rednose pychecker
+    yay -S --needed --noconfirm git emacs ripgrep fd pandoc shellcheck python-pipenv python-isort python-pytest python-rednose pychecker texlive-core
     git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
     ~/.emacs.d/bin/doom install
     export PATH="$PATH":$HOME/.emacs.d/bin
