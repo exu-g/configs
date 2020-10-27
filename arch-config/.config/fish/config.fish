3912d20cb0980f5180e9f065cb712ba07a5487c0
RealStickman <mrcfrm01@protonmail.com>
2020-10-27 17:10:17 +0100

    Specify fish colors

--- arch-config/.config/fish/config.fish
+++ arch-config/.config/fish/config.fish
@@ -2,6 +2,28 @@
 #fish_vi_key_bindings
 fish_default_key_bindings
 
+
+# Info:
+# https://fishshell.com/docs/current/index.html#variables-for-changing-highlighting-colors
+set fish_color_normal blue
+set fish_color_command blue
+set fish_color_quote bryellow
+set fish_color_redirection
+set fish_color_end brred
+set fish_color_error red
+set fish_color_param brblue
+set fish_color_comment white
+set fish_color_selection brcyan
+set fish_color_search_match magenta
+set fish_color_operator blue
+set fish_color_escape green
+set fish_color_cwd yellow
+set fish_color_autosuggestion brwhite
+set fish_color_user yellow
+set fish_color_host brwhite
+set fish_color_host_remote brwhite
+set fish_color_cancel brred
+
 #update config
 alias upconf='~/scripts/arcolinux-config.sh'
 
