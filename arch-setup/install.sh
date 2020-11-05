73562ad421a5e68a8059ae39bffcfdc44a1feb5d
RealStickman <mrcfrm01@protonmail.com>
2020-11-05 13:28:09 +0100

    Add something, idk

--- arch-setup/install.sh
+++ arch-setup/install.sh
@@ -245,7 +245,7 @@ echo Updated packages
 
 #pacman programs
 echo Installing default pacman programs
-sudo pacman -S --needed arandr libreoffice-fresh-de termite wget picom stress obs-studio python-pip hunspell hunspell-de hunspell-en_GB hyphen hyphen-de hyphen-en fish smartmontools thunderbird ffmpeg jre-openjdk thunar gtk-engine-murrine celluloid languagetool rofi nextcloud-client vnstat wireguard-tools cronie libnotify notification-daemon dunst rsync restic piper
+sudo pacman -S --needed arandr libreoffice-fresh-de termite wget picom stress obs-studio python-pip hunspell hunspell-de hunspell-en_GB hyphen hyphen-de hyphen-en fish smartmontools thunderbird ffmpeg jre-openjdk thunar gtk-engine-murrine celluloid languagetool rofi nextcloud-client vnstat wireguard-tools cronie libnotify notification-daemon dunst rsync restic piper lightdm-gtk-greeter unzip
 # REVIEW replace transmission-qt with gtk version
 sudo pacman -S --needed transmission-gtk
 # REVIEW maybe find theme with less dependencies
@@ -265,9 +265,9 @@ echo Installed official programs
 
 # REVIEW Patched neofetch version to remove Color codes
 git clone https://github.com/RealStickman/neofetch
-cd neofetch
+cd neofetch || echo Failed to find the directory
 sudo make install
-cd ..
+cd .. || echo wtf
 rm -rf neofetch
 
 #AUR
