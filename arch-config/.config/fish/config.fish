076e2062a508cc29a34ba425fdc91145a65b5c33
RealStickman <mrcfrm01@protonmail.com>
2020-11-14 10:55:25 +0100

    Add show news for updates

--- arch-config/.config/fish/config.fish
+++ arch-config/.config/fish/config.fish
@@ -69,7 +69,7 @@ alias wget="wget -c"
 
 # Aliases for software managment
 alias pacman='sudo pacman --color auto'
-alias update='yay -Syu --combinedupgrade'
+alias update='yay -Pw && yay -Syu --combinedupgrade'
 
 #grub update
 alias update-grub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
