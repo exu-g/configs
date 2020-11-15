3da26b0f02c645523fbef2e92b66e78444833b67
RealStickman <mrcfrm01@protonmail.com>
2020-11-15 10:53:57 +0100

    Change to fractal

--- arch-config/.config/i3/scripts/layout-default.sh
+++ arch-config/.config/i3/scripts/layout-default.sh
@@ -8,7 +8,7 @@ i3-msg 'workspace 2; layout tabbed; exec thunderbird'
 
 i3-msg 'workspace 2; layout tabbed; exec discord'
 
-i3-msg 'workspace 2; layout tabbed; exec mirage'
+i3-msg 'workspace 2; layout tabbed; exec fractal'
 
 i3-msg 'workspace 2; layout tabbed; exec bettergram'
 
