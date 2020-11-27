0176d920b0ec12d149e27432a99c132e2a4c4ee2
RealStickman <mrcfrm01@protonmail.com>
2020-11-27 11:03:57 +0100

    Replace bettergram with telegram

--- arch-config/.config/i3/scripts/layout-default.sh
+++ arch-config/.config/i3/scripts/layout-default.sh
@@ -10,6 +10,6 @@ i3-msg 'workspace 2; layout tabbed; exec discord'
 
 i3-msg 'workspace 2; layout tabbed; exec fractal'
 
-i3-msg 'workspace 2; layout tabbed; exec bettergram'
+i3-msg 'workspace 2; layout tabbed; exec telegram-desktop'
 
 i3-msg 'mode "default"'
