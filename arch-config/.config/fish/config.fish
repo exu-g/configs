5acd553f7e382da02f78221fab8372c3a3a55b43
RealStickman <mrcfrm01@protonmail.com>
2020-12-29 11:03:25 +0100

    Add Notification after update job

--- arch-config/.config/fish/config.fish
+++ arch-config/.config/fish/config.fish
@@ -85,7 +85,7 @@ alias wget="wget -c"
 # Aliases for software managment
 alias pacman='pacman --color auto'
 #alias update='yay -Syu --sudoloop'
-alias update='paru -Syu --sudoloop --newsonupgrade'
+alias update='paru -Syu --sudoloop --newsonupgrade; notify'
 
 #grub update
 alias update-grub="grub-mkconfig -o /boot/grub/grub.cfg"
