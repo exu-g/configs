9c6008e7f10d78b526b8bf4f8aab97e13bad4a22
RealStickman <mrcfrm01@protonmail.com>
2021-02-12 14:30:17 +0100

    Change to new folder structure

--- arch-config/scripts/pieces/music-create-links.sh
+++ arch-config/scripts/pieces/music-create-links.sh
@@ -8,31 +8,38 @@ rm -rf "$HOME/Musik/"*
 cd "$HOME/MusikRaw"
 
 # get directories
-ls -d */ > directories
+ls -d */ > artistdirectories
 
-while read -r dir; do
-    # change into directory
-    cd "$dir"
-    # create directory in music
-    mkdir -p "$HOME/Musik/$dir"
+while read -r artdir; do
+    cd "$artdir"
+    # get albums
+    ls -d */ > directories
+    while read -r dir; do
+        # change into directory
+        cd "$dir"
+        # create directory in music
+        mkdir -p "$HOME/Musik/${artdir}/$dir"
 
-    # link cover.jpg
-    if [[ -f cover.jpg ]]; then
-        ln -vf "$HOME/MusikRaw/$dir/cover."* "$HOME/Musik/$dir/"
-    fi
+        # link cover.jpg
+        if [[ -f cover.jpg ]]; then
+            ln -vf "$HOME/MusikRaw/${artdir}/$dir/cover."* "$HOME/Musik/${artdir}/$dir/"
+        fi
 
-    # make symbolic link to music
-    # if the "normalized" directory exists, links are created
-    if [[ -d "normalized" ]]; then
-        ln -svf "$HOME/MusikRaw/$dir/normalized/"* "$HOME/Musik/$dir/"
-    fi
+        # make symbolic link to music
+        # if the "normalized" directory exists, links are created
+        if [[ -d "normalized" ]]; then
+            ln -svf "$HOME/MusikRaw/${artdir}/$dir/normalized/"* "$HOME/Musik/${artdir}/$dir/"
+        fi
 
-    # go back to music raw
+        # go back to music raw
+        cd "$HOME/MusikRaw/$artdir"
+    done < directories
+    # cleanup
+    rm directories
     cd "$HOME/MusikRaw"
-done < directories
-
-# remove directories file
-rm directories
+done < artistdirectories
+# cleanup
+rm artistdirectories
 
 echo Finished!
 
