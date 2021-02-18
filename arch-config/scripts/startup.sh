ddf331bf71e4f6b6b262395b316842b58a331e5d
RealStickman <mrcfrm01@protonmail.com>
2021-02-18 21:36:15 +0100

    Remove git pull

--- arch-config/scripts/startup.sh
+++ arch-config/scripts/startup.sh
@@ -5,7 +5,4 @@ set -euo pipefail
 # redetects PulseAudio outputs
 pacmd unload-module module-udev-detect && pacmd load-module module-udev-detect
 
-# Git pull
-bash "$HOME/scripts/git-pull.sh"
-
 exit 0
