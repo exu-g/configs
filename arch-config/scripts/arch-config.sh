3095f864d556c0b1b9a545bd734919b735e634b6
RealStickman <mrcfrm01@protonmail.com>
2021-03-08 09:16:44 +0100

    Add note

--- arch-config/scripts/arch-config.sh
+++ arch-config/scripts/arch-config.sh
@@ -39,6 +39,7 @@ rm -rf ~/config
 git clone https://gitlab.com/RealStickman-arch/config.git &&
 
 # check if the install scripts are the same
+# NOTE Arguments get passed automatically now
 if ! cmp --silent "$HOME/scripts/arch-config.sh" "$HOME/config/scripts/arch-config.sh" ; then
     echo Removed old config file and launched new one.
     rm "$HOME/scripts/arch-config.sh" && cp "$HOME/config/scripts/arch-config.sh" "$HOME/scripts/" && 'bash ~/scripts/arch-config.sh "$@"'
