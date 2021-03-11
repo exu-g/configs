f1185eb2bd9f54d9d7f3008488282226ce31c510
RealStickman <mrcfrm01@protonmail.com>
2021-03-11 15:59:35 +0100

    WIP sed for Distro name

--- arch-config/scripts/arch-config.sh
+++ arch-config/scripts/arch-config.sh
@@ -218,6 +218,7 @@ if [[ "$seltheme" == "nyarch" ]]; then
     cp -r "./themes/nyarch/polybar" "$HOME/.config/"
     cp -r "./themes/nyarch/neofetch/lowpoly_flamegirl_blue.txt" "$HOME/.config/neofetch/lowpoly_flamegirl.txt"
     cp "./themes/.fehbg-nyarch" "$HOME/.fehbg"
+    #sed -i 's/^NAME=".*"/NAME="Rawrch Linyux"/' /etc/os-release
 elif [[ "$seltheme" == "space-pink" ]]; then
     cp -r "./themes/space-pink/i3" "$HOME/.config/"
     cp -r "./themes/space-pink/polybar" "$HOME/.config/"
