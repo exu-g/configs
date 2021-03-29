33a96778e5c869fe97a13279b8d90750240e336a
RealStickman <mrc@frm01.net>
2021-03-29 19:22:52 +0200

    Add sleep for layouts

--- arch-config/.config/i3/scripts/layout-default.sh
+++ arch-config/.config/i3/scripts/layout-default.sh
@@ -2,14 +2,24 @@
 
 i3-msg 'workspace 1; exec kitty'
 
+sleep 0.5
+
 i3-msg 'workspace 2; layout tabbed; exec firefox'
+
+sleep 0.5
 
 i3-msg 'workspace 2; layout tabbed; exec thunderbird'
 
+sleep 0.5
+
 i3-msg 'workspace 2; layout tabbed; exec discord'
+
+sleep 0.5
 
 #i3-msg 'workspace 2; layout tabbed; exec element-desktop'
 
 i3-msg 'workspace 2; layout tabbed; exec telegram-desktop'
+
+sleep 0.5
 
 i3-msg 'mode "default"'
--- arch-config/.config/i3/scripts/layout-work.sh
+++ arch-config/.config/i3/scripts/layout-work.sh
@@ -2,14 +2,26 @@
 
 i3-msg 'workspace 1; exec kitty'
 
+sleep 0.5
+
 i3-msg 'workspace 2; layout tabbed; exec firefox'
+
+sleep 0.5
 
 i3-msg 'workspace 2; layout tabbed; exec thunderbird'
 
+sleep 0.5
+
 i3-msg 'workspace 2; layout tabbed; exec slack'
+
+sleep 0.5
 
 i3-msg 'workspace 2; layout tabbed; exec teams'
 
+sleep 0.5
+
 i3-msg 'workspace 2; layout tabbed; exec p3x-onenote'
+
+sleep 0.5
 
 i3-msg 'mode "default"'
