33dc4c76b34a4810dcb7b0eb4ef38d6a6088d214
RealStickman <mrc@frm01.net>
2021-04-03 08:44:35 +0200

    smaller intervals

--- arch-config/.config/i3/scripts/layout-default.sh
+++ arch-config/.config/i3/scripts/layout-default.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
+i3-msg 'mode "default"'
+
 i3-msg 'workspace 1; exec kitty'
 
-sleep 0.5
+sleep 0.4
 
 i3-msg 'workspace 2; layout tabbed; exec firefox'
 
-sleep 0.5
+sleep 0.3
 
 i3-msg 'workspace 2; layout tabbed; exec thunderbird'
 
-sleep 0.5
+#sleep 0.5
 
 i3-msg 'workspace 2; layout tabbed; exec discord'
 
-sleep 0.5
+sleep 0.7
 
 #i3-msg 'workspace 2; layout tabbed; exec element-desktop'
 
 i3-msg 'workspace 2; layout tabbed; exec telegram-desktop'
-
-sleep 0.5
-
-i3-msg 'mode "default"'
