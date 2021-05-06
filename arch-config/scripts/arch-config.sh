53aa175635dfb040b73fa196c29c3d4567af56d5
RealStickman <mrc@frm01.net>
2021-05-06 19:09:38 +0200

    Silent output

--- arch-config/scripts/arch-config.sh
+++ arch-config/scripts/arch-config.sh
@@ -320,7 +320,7 @@ fi
 # ps aux | grep "\si3\s" breaks if i3 hasn't been restarted yet
 # ps aux | grep "\si3" works for both, not certain if other stuff could be detected as well
 # ps aux | grep "\si3\$" breaks if i3 has been restarted already in this session
-if ps aux | grep -E "\si3(\s|$)"; then
+if ps aux | grep -E "\si3(\s|$)" &>/dev/null; then
     i3-msg restart
 fi
 
