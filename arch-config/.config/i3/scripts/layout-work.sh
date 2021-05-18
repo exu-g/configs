9ca993148c5da9183c94e7a2c5cb511edf97a4a1
RealStickman <mrc@frm01.net>
2021-05-18 10:02:06 +0200

    Change timings for work layout

--- arch-config/.config/i3/scripts/layout-work.sh
+++ arch-config/.config/i3/scripts/layout-work.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+i3-msg 'mode "default"'
+
 i3-msg 'workspace 1; exec kitty'
 
-sleep 0.5
+sleep 0.4
 
 i3-msg 'workspace 2; layout tabbed; exec firefox'
 
-sleep 0.5
+sleep 0.3
 
 i3-msg 'workspace 2; layout tabbed; exec thunderbird'
 
-sleep 0.5
+sleep 2
 
 i3-msg 'workspace 2; layout tabbed; exec slack'
 
@@ -18,10 +20,7 @@ sleep 0.5
 
 i3-msg 'workspace 2; layout tabbed; exec teams'
 
-sleep 0.5
+sleep 1
 
 i3-msg 'workspace 2; layout tabbed; exec p3x-onenote'
 
-sleep 0.5
-
-i3-msg 'mode "default"'
