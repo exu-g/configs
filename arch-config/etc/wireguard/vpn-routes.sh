2a20eed426fd779e9a2b1e206a61237743e9d376
RealStickman <mrc@frm01.net>
2021-06-11 13:35:36 +0200

    Add comment

--- arch-config/etc/wireguard/vpn-routes.sh
+++ arch-config/etc/wireguard/vpn-routes.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 # creates an array of all network devices
+# NOTE only matches wlp[0-9]s[0-9].* and enp[0-9]s[0-9].*
 #readarray -t interfaces < <(ip l | awk -F ":" '/^[0-9]+:/{dev=$2 ; if ( dev !~ /^ lo$/ && dev !~ /^ vmnet.*/) {print $2}}')
 readarray -t interfaces < <(ip l | awk -F ":" '/^[0-9]+:/{dev=$2 ; if ( dev ~ /^ wlp[0-9]s[0-9].*$/ || dev ~ /^ enp[0-9]s[0-9].*/) {print $2}}')
 
