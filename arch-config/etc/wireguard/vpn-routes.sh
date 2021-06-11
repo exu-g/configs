b46420a80b2efb893901a54dc0e32abc2af01fdb
RealStickman <mrc@frm01.net>
2021-06-11 13:34:47 +0200

    Read matching interfaces to array

---
+++ arch-config/etc/wireguard/vpn-routes.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# creates an array of all network devices
+#readarray -t interfaces < <(ip l | awk -F ":" '/^[0-9]+:/{dev=$2 ; if ( dev !~ /^ lo$/ && dev !~ /^ vmnet.*/) {print $2}}')
+readarray -t interfaces < <(ip l | awk -F ":" '/^[0-9]+:/{dev=$2 ; if ( dev ~ /^ wlp[0-9]s[0-9].*$/ || dev ~ /^ enp[0-9]s[0-9].*/) {print $2}}')
+
+# print array
+for i in "${interfaces[@]// /}" ; do echo "$i" ; done
