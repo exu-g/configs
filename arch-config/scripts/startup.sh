983084cdf78700e67c405a4690a58bc8c7eb6753
RealStickman <mrc@frm01.net>
2021-07-15 15:47:35 +0200

    Comment this stuff. Not needed anymore with pipewire

--- arch-config/scripts/startup.sh
+++ arch-config/scripts/startup.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 # redetects PulseAudio outputs
-pacmd unload-module module-udev-detect && pacmd load-module module-udev-detect
+#pacmd unload-module module-udev-detect && pacmd load-module module-udev-detect
 
 exit 0
