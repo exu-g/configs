cc2ad7e6a31c7397687e0506ba2951ca71e45a52
RealStickman <mrc@frm01.net>
2021-08-23 20:38:27 +0200

    Use local dns servers first

--- arch-config/scripts/pieces/wireguard-add-route-dns.sh
+++ arch-config/scripts/pieces/wireguard-add-route-dns.sh
@@ -36,6 +36,9 @@ for file in "${conffiles[@]}"; do
         # TODO tables do not work as intended
         #awk 'NR==5{print "PostUp = ip route add 192.168.1.0/24 via 192.168.86.1 metric 10 table 7"}NR==5{print "PreDown = ip route flush table 7"}1' "$file" > "${file}.tmp"
         # NOTE adds a dns to all configs
+        awk 'NR==4{print "DNS = 172.16.52.5"}1' "$file" > "${file}.tmp"
+        mv "${file}.tmp" "$file"
+        # NOTE adds a dns to all configs
         awk 'NR==5{print "DNS = 172.16.16.1"}1' "$file" > "${file}.tmp"
         mv "${file}.tmp" "$file"
         # wifi
