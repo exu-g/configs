b78bc9d13039b426e024b0a17dec73ff398e3802
RealStickman <mrc@frm01.net>
2021-09-01 18:33:13 +0200

    Add joplin. Change order for setting tabbed layout

--- arch-config/.config/i3/scripts/layout-default.sh
+++ arch-config/.config/i3/scripts/layout-default.sh
@@ -6,23 +6,24 @@ i3-msg 'workspace 1; exec kitty'
 
 sleep 0.4
 
-i3-msg 'workspace 2; layout tabbed'
-
-sleep 0.1
-
 i3-msg 'workspace 2; exec firefox'
 
-sleep 0.9
+sleep 1.0
 
-#i3-msg 'workspace 2; layout tabbed; exec thunderbird'
 i3-msg 'workspace 2; exec evolution'
+
+sleep 0.5
+
+i3-msg 'workspace 2; exec joplin-desktop'
 
 sleep 0.3
 
 i3-msg 'workspace 2; exec discord'
 
-sleep 5.8
-
-#i3-msg 'workspace 2; layout tabbed; exec element-desktop'
+sleep 6.0
 
 i3-msg 'workspace 2; exec telegram-desktop'
+
+sleep 1
+
+i3-msg 'workspace 2; layout tabbed'
--- arch-config/.config/i3/scripts/layout-work.sh
+++ arch-config/.config/i3/scripts/layout-work.sh
@@ -6,25 +6,24 @@ i3-msg 'workspace 1; exec kitty'
 
 sleep 0.4
 
-i3-msg 'workspace 2; layout tabbed'
-
-sleep 0.1
-
 i3-msg 'workspace 2; exec firefox'
 
-sleep 0.9
+sleep 1.0
 
-#i3-msg 'workspace 2; layout tabbed; exec thunderbird'
 i3-msg 'workspace 2; exec evolution'
 
-sleep 2
+sleep 0.5
 
-#i3-msg 'workspace 2; layout tabbed; exec slack'
+i3-msg 'workspace 2; exec joplin-desktop'
 
-#sleep 0.5
+sleep 0.5
 
 i3-msg 'workspace 2; exec teams'
 
 sleep 1
 
 i3-msg 'workspace 2; exec p3x-onenote'
+
+sleep 1
+
+i3-msg 'workspace 2; layout tabbed'
