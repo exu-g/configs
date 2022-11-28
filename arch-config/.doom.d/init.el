03bdc17eaf7f409e0ffae972490206254a891bae
RealStickman <mrc@frm01.net>
2022-11-28 11:43:02 +0000

    Add fish support

--- arch-config/.doom.d/init.el
+++ arch-config/.doom.d/init.el
@@ -161,7 +161,7 @@
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;scheme            ; a fully conniving family of lisps
-       (sh +powershell +lsp)         ; she sells {ba,z,fi}sh shells on the C xor
+       (sh +powershell +fish +lsp)         ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
