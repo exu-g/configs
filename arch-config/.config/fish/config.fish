9f338b257781e829c6db9a650eb142a4b779dbf6
RealStickman <mrc@frm01.net>
2023-02-01 19:11:21 +0100

    Modify yt-dlp aliases

--- arch-config/.config/fish/config.fish
+++ arch-config/.config/fish/config.fish
@@ -156,16 +156,19 @@ alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacma
 #alias vbm="sudo mount -t vboxsf -o rw,uid=1000,gid=1000 Public /home/$USER/Public"
 
 #youtube-dl
-alias yta-aac="youtube-dl --extract-audio --audio-format aac "
-alias yta-best="youtube-dl --extract-audio --audio-format best "
-alias yta-flac="youtube-dl --extract-audio --audio-format flac "
-alias yta-m4a="youtube-dl --extract-audio --audio-format m4a "
-alias yta-mp3="youtube-dl --extract-audio --audio-format mp3 "
-alias yta-opus="youtube-dl --extract-audio --audio-format opus "
-alias yta-vorbis="youtube-dl --extract-audio --audio-format vorbis "
-alias yta-wav="youtube-dl --extract-audio --audio-format wav "
+#alias yta-aac="youtube-dl --extract-audio --audio-format aac "
+#alias yta-best="youtube-dl --extract-audio --audio-format best "
+#alias yta-flac="youtube-dl --extract-audio --audio-format flac "
+#alias yta-m4a="youtube-dl --extract-audio --audio-format m4a "
+#alias yta-mp3="youtube-dl --extract-audio --audio-format mp3 "
+#alias yta-opus="youtube-dl --extract-audio --audio-format opus "
+#alias yta-vorbis="youtube-dl --extract-audio --audio-format vorbis "
+#alias yta-wav="youtube-dl --extract-audio --audio-format wav "
 
-alias ytv-best="youtube-dl -f bestvideo+bestaudio "
+# yt-dlp
+alias yta-best="yt-dlp -f bestaudio --extract-audio "
+alias ytv-best="yt-dlp -f bestvideo+bestaudio "
+alias ytv-complete="yt-dlp -f bestvideo+bestaudio --write-info-json --write-subs --sub-langs \"en.*\"  "
 
 #Cleanup orphaned packages
 alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
