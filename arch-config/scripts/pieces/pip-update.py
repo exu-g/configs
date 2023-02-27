2020a586ec8964a7df84368780c12bd0f34918b4
RealStickman <mrc@frm01.net>
2023-02-27 21:49:53 +0100

    Fix pip package update script

--- arch-config/scripts/pieces/pip-update.py
+++ arch-config/scripts/pieces/pip-update.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 import pkg_resources
-from subprocess import call
+import subprocess
 
+# get a list of all packages installed
 packages = [dist.project_name for dist in pkg_resources.working_set]
-call("pip install --upgrade " + ' '.join(packages), shell=True)
+
+# update each package
+for package in packages:
+    subprocess.call("pip install --upgrade {}".format(package), shell=True)
