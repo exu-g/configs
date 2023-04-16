1a402b6a57efa1e2337e51627db2b3c8c16efaba
RealStickman <mrc@frm01.net>
2023-04-16 20:24:48 +0200

    System package list

---
+++ nixos-config/system-packages.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+
+{
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    hyprland
+    wayland
+    xdg-utils
+  ];
+
+}
