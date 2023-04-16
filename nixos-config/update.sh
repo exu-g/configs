4bc75feb974be0062e1ad58309798d37964dc04f
RealStickman <mrc@frm01.net>
2023-04-16 20:34:42 +0200

    Use git to update configs

--- nixos-config/update.sh
+++ nixos-config/update.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 set -euo pipefail
 
-curl -O https://gitea.exu.li/RealStickman/nixos-config/raw/branch/main/configuration.nix
-curl -O https://gitea.exu.li/RealStickman/nixos-config/raw/branch/main/home-manager.nix
-curl -O https://gitea.exu.li/RealStickman/nixos-config/raw/branch/main/system-packages.nix
+git fetch -a
+git reset --hard
+git clean -fd
+git pull
