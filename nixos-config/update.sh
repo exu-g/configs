22cfbb58f763fd7ee899c12977be63e34f9d9d69
RealStickman <mrc@frm01.net>
2023-04-16 20:25:58 +0200

    Add update script

---
+++ nixos-config/update.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+set -euo pipefail
+
+curl -O https://gitea.exu.li/RealStickman/nixos-config/raw/branch/main/configuration.nix
+curl -O https://gitea.exu.li/RealStickman/nixos-config/raw/branch/main/home-manager.nix
+curl -O https://gitea.exu.li/RealStickman/nixos-config/raw/branch/main/system-packages.nix
