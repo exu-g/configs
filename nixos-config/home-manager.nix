af8d59b8047032b246a1ba1b3d5b18d065413e5f
RealStickman <mrc@frm01.net>
2023-04-16 19:29:38 +0200

    Add Home-Manager config file

---
+++ nixos-config/home-manager.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+
+let
+  user = "exu";
+  hostname = "nixos";
+in
+{
+  imports = [
+    <home-manager/nixos>
+  ];
+
+  home-manager.users.${user} = { pkgs, ... }: {
+    home.stateVersion = "22.11";
+    home.packages = [
+      pkgs.firefox
+      pkgs.kitty
+      pkgs.pciutils
+      pkgs.wget
+    ];
+  };
+}
