c63cb54207013064aaa1f7bec1ac0784e70f9d95
RealStickman <mrc@frm01.net>
2023-04-16 20:51:27 +0200

    Start adding more packages

--- nixos-config/home-manager.nix
+++ nixos-config/home-manager.nix
@@ -16,6 +16,15 @@ in
       pkgs.kitty # terminal
       pkgs.pciutils # lspci command
       pkgs.git # git
+      pkgs.emacsPackages.doom # doom emacs
+      pkgs.acpilight # controlling laptop monitor backlight
+      pkgs.networkmanagerapplet # network configuration
+      pkgs.wofi # app launcher (wayland replacement for rofi)
+      pkgs.freetype # font rendering and configuration
+      pkgs.fira # fira sans font
+      pkgs.fira-code # fira code font
+      pkgs.fwupd # firmware updates
+      pkgs.fwupd-efi # firmware updates additional EFI stuff
     ];
   };
 }
