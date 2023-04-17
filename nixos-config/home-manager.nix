aeb115228cafa12eebeb446c458c601d332f87d2
RealStickman <mrc@frm01.net>
2023-04-17 21:29:17 +0200

    Remove end, will be automatically added

--- nixos-config/home-manager.nix
+++ nixos-config/home-manager.nix
@@ -69,7 +69,6 @@ set fish_greeting "Good Morning! Nice day for fishing ain't it! Hu ha!"
       set -l prompt_status (__fish_print_pipestatus "[" "]" "|" (set_color $fish_color_status) (set_color $bold_flag $fish_color_status) $last_pipestatus)
 
       echo -n -s (set_color $fish_color_user) "$USER" $normal (set_color $fish_color_separator) @ $normal (set_color $color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
-  end
   '';
         };
       };
