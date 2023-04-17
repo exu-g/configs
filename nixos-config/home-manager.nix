34923dfa33e21523c1b9f17e7f74ba3b07045e1c
RealStickman <mrc@frm01.net>
2023-04-17 20:45:22 +0200

    move stuff

--- nixos-config/home-manager.nix
+++ nixos-config/home-manager.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, home, ... }:
 
 let
   user = "exu";
@@ -47,18 +47,9 @@ in
     # Color the prompt differently when we're root
     set -l color_cwd $fish_color_cwd
     set -l suffix '>'
-    #if functions -q fish_is_root_user; and fish_is_root_user
-    #    if set -q fish_color_cwd_root
-    #        set color_cwd $fish_color_cwd_root
-    #    end
-    #    set suffix '#'
-    #end
 
     # If we're running via SSH, change the host color.
     set -l color_host $fish_color_host
-    #if set -q SSH_TTY
-    #    set color_host $fish_color_host_remote
-    #end
 
     # Write pipestatus
     # If the status was carried over (e.g. after `set`), don't bold it.
@@ -71,7 +62,6 @@ in
     set -l prompt_status (__fish_print_pipestatus "[" "]" "|" (set_color $fish_color_status) (set_color $bold_flag $fish_color_status) $last_pipestatus)
 
     echo -n -s (set_color $fish_color_user) "$USER" $normal (set_color $fish_color_separator) @ $normal (set_color $color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
-    #echo -n -s (set_color --bold red) "$USER" $normal (set_color --bold yellow) @ $normal (set_color cyan) (prompt_hostname) $normal ' ' (set_color yellow) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
 end
 '';
       };
