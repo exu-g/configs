ee2a38c22fdb203140b325abd0745edc2e18b3c5
RealStickman <mrc@frm01.net>
2023-05-01 13:23:52 +0200

    reformat variable definitions

--- nixos-config/home-manager/hyprland.nix
+++ nixos-config/home-manager/hyprland.nix
@@ -1,10 +1,13 @@
-{pkgs, ...}: let
+{pkgs, ...}:
+
+let
   flake-compat = builtins.fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz";
 
   hyprland = (import flake-compat {
     src = builtins.fetchTarball "https://github.com/hyprwm/Hyprland/archive/master.tar.gz";
   }).defaultNix;
-in {
+in
+{
   imports = [
     hyprland.homeManagerModules.default
   ];
