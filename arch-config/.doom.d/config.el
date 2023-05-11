1c2a52f68d67af5ef246899a4e281ea823504476
RealStickman <mrc@frm01.net>
2023-05-11 16:26:25 +0200

    Revert "Enable crystal with crystalline as lsp"
    This reverts commit 3bf3f0a4368fdc3579553c18cc18c775e6eb8d02.

--- arch-config/.doom.d/config.el
+++ arch-config/.doom.d/config.el
@@ -85,13 +85,3 @@
 ;; work around emacs hanging when opening any python requirements.txt file
 ;; see issue https://github.com/doomemacs/doomemacs/issues/5998
 (advice-add #'pip-requirements-fetch-packages :override #'ignore)
-
-;; enable crystalline lsp for crystal-mode
-(with-eval-after-load 'lsp-mode
-  (add-to-list 'lsp-language-id-configuration
-               '(crystal-mode . "crystal"))
-  (lsp-register-client
-  (make-lsp-client :new-connection (lsp-stdio-connection '("crystalline"))
-                   :activation-fn (lsp-activate-on "crystal")
-                   :priority '1
-                   :server-id 'crystalline)))
