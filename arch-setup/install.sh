df57ca3a0fd1a83b7424db3e97b6df4919995f9e
exu <mrc@frm01.net>
2023-11-17 17:24:15 +0100

    Remove not found depencies

--- arch-setup/install.sh
+++ arch-setup/install.sh
@@ -319,7 +319,8 @@ fi
 #devtools
 if [ $in_doomemacs -eq 1 ]; then
     echo "Installing doom-emacs"
-    paru -S --needed git emacs ripgrep fd pandoc shellcheck python-pipenv python-isort python-pytest python-rednose pychecker texlive-core python-black clang
+    #paru -S --needed git emacs ripgrep fd pandoc shellcheck python-pipenv python-isort python-pytest python-rednose pychecker texlive-core python-black clang
+    paru -S --needed git emacs ripgrep fd pandoc shellcheck python-pipenv python-isort python-pytest texlive-core python-black clang
     pip install grip
     npm i bash-language-server
     git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
