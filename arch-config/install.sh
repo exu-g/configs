6c9c9d5b8c2f713ff3babdea7538cd742f807862
exu <mrc@frm01.net>
2023-11-17 19:43:41 +0100

    Make config install script independent of path

--- arch-config/install.sh
+++ arch-config/install.sh
@@ -3,12 +3,13 @@ set -euo pipefail
 
 #DO NOT MAKE CHANGES TO THE INSTALL SCRIPT HERE. USE arch-config.sh IN ~/scripts FOR THAT
 
-#change to home (does not show in terminal)
-cd $HOME
+# get script directory
+scriptloc="$BASH_SOURCE"
+configdir=$(dirname "$scriptloc")
 
 echo Launching arch-config.sh
 
 #make executable & launch arch-config.sh
-chmod +x ~/configs/arch-config/scripts/arch-config.sh
-bash ~/configs/arch-config/scripts/arch-config.sh
+chmod +x "$configdir/scripts/arch-config.sh"
+bash "$configdir/scripts/arch-config.sh"
 exit 0
