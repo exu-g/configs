04f83567e770a612bd265890e21af43989ab3cf8
exu <mrc@frm01.net>
2023-11-17 20:03:20 +0100

    Create autostart directory

--- arch-config/scripts/arch-config.sh
+++ arch-config/scripts/arch-config.sh
@@ -364,6 +364,9 @@ cat <<EOF
 ########################################
 EOF
 
+# create autostart directory
+mkdir -p ~/.config/autostart/
+
 # copy corectrl desktop file
 if [[ $(pacman -Q | grep corectrl) ]]; then
     cp /usr/share/applications/org.corectrl.corectrl.desktop ~/.config/autostart/org.corectrl.corectrl.desktop
