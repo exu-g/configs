9a00a664fee0b828c435bb8749dcaa0cead32b22
exu <mrc@frm01.net>
2024-10-08 10:00:42 +0200

    Add fish function for updating pip packages

---
+++ arch-config/.config/fish/functions/pipup.fish
@@ -0,0 +1,15 @@
+function pipup
+    switch $argv[1]
+        case 'update'
+            python -m pip list --outdated --format=json | python -c "import json, sys; print('\n'.join([x['name'] for x in json.load(sys.stdin)]))" | xargs -n1 python -m pip install -U
+        case 'list'
+            python -m pip list --outdated
+        case 'help'
+            echo "Help:"
+            echo "- update: Updates all installed Python packages"
+            echo "- list:   Lists all Python packages that need updates"
+        case '*'
+            echo "Unknown command: $argv[1]"
+            echo "Use `help` to see available commands"
+    end
+end
