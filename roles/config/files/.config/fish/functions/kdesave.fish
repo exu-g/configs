03e644c25c9901ae53b3ec66f843e20aabf9fe8b
exu <mrc@frm01.net>
2024-10-23 18:47:56 +0200

    Use system konsave

--- roles/config/files/.config/fish/functions/kdesave.fish
+++ roles/config/files/.config/fish/functions/kdesave.fish
@@ -1,9 +1,8 @@
 # save kde and plasma settings to gitprojects/configs
 function kdesave
-    rm -rf ~/.config/konsave
     rm -rf ./konsave-export-main ./konsave-export-main.knsv
-    ~/gitprojects/konsave/venv/bin/python -m konsave --save konsave-export-main --force
-    ~/gitprojects/konsave/venv/bin/python -m konsave --export-profile konsave-export-main
+    konsave --save konsave-export-main --force
+    konsave --export-profile konsave-export-main
     unzip konsave-export-main.knsv -d konsave-export-main
     cp -r ./konsave-export-main/save/configs/* ~/gitprojects/configs/roles/config/files/.config/
     cp -r ./konsave-export-main/export/home_folder/.* ~/gitprojects/configs/roles/config/files/
