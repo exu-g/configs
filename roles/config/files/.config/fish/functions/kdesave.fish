274f60973c9bf265cb6c339b90ffa5435b8c8d1e
exu <mrc@frm01.net>
2025-09-14 18:55:05 +0200

    update more kde settings

--- roles/config/files/.config/fish/functions/kdesave.fish
+++ roles/config/files/.config/fish/functions/kdesave.fish
@@ -8,6 +8,10 @@ function kdesave
     #cp -r ./konsave-export-main/export/home_folder/.* ~/gitprojects/configs/roles/config/files/
     cp -r ./konsave-export-main/export/share_folder/* ~/gitprojects/configs/roles/config/files/.local/share/
     rm -rf ./konsave-export-main ./konsave-export-main.knsv
-    # kdeconnect somehow gets copied every time
+    # Remove some files
     rm -rf ~/gitprojects/configs/roles/config/files/.config/kdeconnect/
+    rm -rf ~/gitprojects/configs/roles/config/files/.config/kate/
+    rm -f ~/gitprojects/configs/roles/config/files/.config/katerc
+    rm -f ~/gitprojects/configs/roles/config/files/.config/katevirc
+    rm -f ~/gitprojects/configs/roles/config/files/.config/kde.org/UserFeedback*
 end
