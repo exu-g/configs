02650aa2172c68b1ca6f697e3ea7b18fdc0bdb05
exu <mrc@frm01.net>
2025-12-17 18:49:58 +0100

    improve script to delete normalized directories

--- roles/config/files/scripts/pieces/music-delete-normalized.sh
+++ roles/config/files/scripts/pieces/music-delete-normalized.sh
@@ -1,27 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# change into music raw folder
-cd "$HOME/Nextcloud/MusikRaw"
+find "$HOME/Nextcloud/MusikRaw" -depth -name "normalized" -type d -exec rm -r '{}' \;
 
-# get directories
-ls -d */ > artistdirectories
-
-while read -r artdir; do
-    cd "$artdir"
-    # get albums
-    ls -d */ > directories
-    # delete normalized from albums
-    while read -r dir; do
-        cd "$dir"
-        rm -rf normalized
-        cd "$HOME/Nextcloud/MusikRaw/$artdir"
-    done < directories
-    # cleanup
-    rm directories
-    cd "$HOME/Nextcloud/MusikRaw"
-done < artistdirectories
-# cleanup
-rm artistdirectories
-
-exit
+echo 'Removed all "normalized" directories and files'
