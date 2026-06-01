a0477b16a992b1bf3cdebee8d7349bacbb872940
exu <mrc@frm01.net>
2026-06-01 13:51:30 +0200

    fish: expand megapull script a bit

--- roles/config/files/.config/fish/functions/megapull.fish
+++ roles/config/files/.config/fish/functions/megapull.fish
@@ -1,4 +1,16 @@
 # Pull all git repos within the current directory
 function megapull
-    find . -type d -exec test -d {}/.git \; -exec echo "Working on {}" \; -exec git -C {} pull \;
+    switch $argv[1]
+        case main
+            find . -type d -exec test -d {}/.git \; -exec echo "Working on {}" \; -exec git -C {} switch main \; -exec git -C {} pull \;
+        case master
+            find . -type d -exec test -d {}/.git \; -exec echo "Working on {}" \; -exec git -C {} switch master \; -exec git -C {} pull \;
+        case help
+            echo "Help:"
+            echo "- <default>: Recursively find git repos and git pull"
+            echo "- master:    Switch to `master` branch and git pull"
+            echo "- main:      Switch to `main` branch and git pull"
+        case '*'
+            find . -type d -exec test -d {}/.git \; -exec echo "Working on {}" \; -exec git -C {} pull \;
+    end
 end
