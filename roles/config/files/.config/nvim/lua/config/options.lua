99fd9ed670de476ad73a2cdcf068ca8e287fcc0d
exu <mrc@frm01.net>
2026-09-17 08:54:33 +0200

    change neovim font

--- roles/config/files/.config/nvim/lua/config/options.lua
+++ roles/config/files/.config/nvim/lua/config/options.lua
@@ -2,8 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Set GUI font to monospace with size 11
-vim.opt.guifont = "Monospace:h11"
+-- Set GUI font, see https://neovim.io/doc/user/options/#'guifont'
+vim.opt.guifont = "Maple Mono NL NF:h12"
 
 -- Enable virtual lines for lsp errors
 vim.diagnostic.config({ virtual_lines = true })
